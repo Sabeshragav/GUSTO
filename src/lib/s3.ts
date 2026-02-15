0f4240931d4c10492fdece04dc4f7f8a60992ea1
Sabeshragav <sabeshragav289@gmail.com>
2026-02-15 13:15:47 +0530

    feat: add S3 utility for uploading files to a specified bucket.

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,14 +1,14 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const s3Client = new S3Client({
-    region: process.env.AWS_S3_REGION!,
+    region: process.env.APP_AWS_S3_REGION!,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY!,
     },
 });
 
-const BUCKET = process.env.AWS_S3_BUCKET!;
+const BUCKET = process.env.APP_AWS_S3_BUCKET!;
 
 /**
  * Upload a file to S3.
@@ -33,5 +33,5 @@ export async function uploadToS3(
         })
     );
 
-    return `https://${BUCKET}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${key}`;
+    return `https://${BUCKET}.s3.${process.env.APP_AWS_S3_REGION}.amazonaws.com/${key}`;
 }
